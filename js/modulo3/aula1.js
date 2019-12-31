346811fd9a492b1367670b06f704d5ce8d9fc09c
Mayra <mayracnx@gmail.com>
2019-12-31 01:56:19 -0300

    adiciona mais codigo do modulo 3

--- js/modulo3/aula1.js
+++ js/modulo3/aula1.js
@@ -8,27 +8,38 @@ var toDos = [
      */
     'Fazer café',
     'Estudar JavaScript',
-    'Acessar comunidade da Rcoketseat'
+    'Acessar comunidade da Rocketseat'
 ];
 
 function renderToDos(){
-    for (toDo of toDos){
-        var toDoElement = document.createElement('li');
-        var toDoText = document.createTextNode(toDo);
 
-        toDoElement.appendChild(toDoText);
-        listElement.appendChild(toDoElement);
+    listElement.innerHTML = '';
+
+    for (toDo of toDos){
+        var toDoElement = document.createElement('li'); // cria um item na lista
+        var toDoText = document.createTextNode(toDo); // coloca o texto do todo na variavel todotext
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        var linkText = document.createTextNode('Excluir');
+
+        linkElement.appendChild(linkText);
+
+        toDoElement.appendChild(toDoText); // adiciona o texto ao elemento todo (o li)
+        toDoElement.appendChild(linkElement);
+        listElement.appendChild(toDoElement); // adiciona o elemento todo na lista 
 
     }
 }
 
+renderToDos();
 
 function addToDo(){
-    var toDoText = inputElement.value;
+    var toDoText = inputElement.value; // pega o valor do input e coloca na variavel
 
-    toDos.push(toDoText);
-    inputElement.value = '';
-    renderToDos();
+    toDos.push(toDoText); // adiciona a variavel q acabou de ser criada ao array (no final)
+    inputElement.value = ''; // limpa o input
+    renderToDos(); // renderiza novamente para aparecer o novo item acrescentado
 
 }
 
